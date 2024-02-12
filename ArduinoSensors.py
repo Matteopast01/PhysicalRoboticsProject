c0f3b144e5390bd6ec82ced6ad67ec92fa5da4dc
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-02-12 16:43:06 +0100

    some changes and added ArduinoSensors.py class for right and left sensors and compass

---
+++ ArduinoSensors.py
@@ -0,0 +1,41 @@
+import serial
+
+SERIAL_PORT = '/dev/ttyUSB0'  # must be changed with a different configuration
+
+
+class SensorAndCamera:
+    # ultrasonic sensors
+    _right_sensor: float
+    _left_sensor: float
+    # compass sensors
+    _orientation: float
+
+    def __init__(self):
+        self._right_sensor = 0
+        self._left_sensor = 0
+        self._orientation = 0
+        self.update()
+
+    def _set_values(self, right_sensor, left_sensor, orientation):
+        self._right_sensor = right_sensor
+        self._left_sensor = left_sensor
+        self._orientation = orientation
+
+    def update(self):
+        # read from sensors
+        ser = serial.Serial(SERIAL_PORT, 9600)
+        # line read
+        line = ser.readline().decode('utf-8').strip()
+        # splitting
+        values_list = line[1:].split("#")
+        # setters
+        self._set_values(float(values_list[0]), float(values_list[1]), float(values_list[2]))
+
+    def get_right_sensor(self):
+        return self._right_sensor
+
+    def get_left_sensor(self):
+        return self._left_sensor
+
+    def get_orientation(self):
+        return self._orientation
