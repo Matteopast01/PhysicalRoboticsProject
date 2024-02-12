d9608f22b4a4117547fd8a58a05544d5d1a8a143
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-02-12 12:28:01 +0100

    comment modified, added usb name in raspberry

--- Sensor_and_camera_reading.py
+++ Sensor_and_camera_reading.py
@@ -2,6 +2,7 @@ import serial
 
 # Imposta la porta seriale e la velocità di trasmissione (baud rate)
 # modificare la stringa '/dev/tty.usbserial-140' con quella presente nella configurazione finale
+# /dev/ttyUSB0 è quella in alto a destra, vista dal raspberry
 ser = serial.Serial('/dev/tty.usbserial-140',
                     9600)
 
