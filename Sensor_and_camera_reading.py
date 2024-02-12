badd7976ecee1ecd98850706844a9ba3c607d9f8
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-02-12 12:16:18 +0100

    Created sensor_reading_and_camera_reading.py to read from serial port from Arduino

---
+++ Sensor_and_camera_reading.py
@@ -0,0 +1,18 @@
+import serial
+
+# Imposta la porta seriale e la velocità di trasmissione (baud rate)
+# modificare la stringa '/dev/tty.usbserial-140' con quella presente nella configurazione finale
+ser = serial.Serial('/dev/tty.usbserial-140',
+                    9600)
+
+try:
+    while True:
+        # Leggi una riga dalla seriale
+        line = ser.readline().decode('utf-8').strip()
+        # Stampa la riga letta
+        print(line)
+
+except KeyboardInterrupt:
+    # Chiudi la porta seriale in caso di interruzione manuale (Ctrl+C)
+    ser.close()
+    print("Chiusura della connessione seriale.")
