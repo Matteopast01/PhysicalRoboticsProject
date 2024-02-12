817f9ae920a9e2c6cbb20f42979de27e6ad6c3b1
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-02-12 12:40:24 +0100

    added Sensor and camera documentation and modified serial string with the Raspberry one. Not yet checked the reading from Raspberry

--- Sensor_and_camera_reading.py
+++ Sensor_and_camera_reading.py
@@ -1,9 +1,10 @@
 import serial
 
 # Imposta la porta seriale e la velocità di trasmissione (baud rate)
-# modificare la stringa '/dev/tty.usbserial-140' con quella presente nella configurazione finale
-# /dev/ttyUSB0 è quella in alto a destra, vista dal raspberry
-ser = serial.Serial('/dev/tty.usbserial-140',
+# modificare la stringa con quella presente nella configurazione finale
+# '/dev/tty.usbserial-140' è quella del mac di Giacomo
+# '/dev/ttyUSB0' è quella del Raspberry in alto a destra
+ser = serial.Serial('/dev/ttyUSB0',
                     9600)
 
 try:
