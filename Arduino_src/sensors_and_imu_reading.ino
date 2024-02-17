7335d869d42fbf6bf4911a1a02d6cd796fc6ff55
Matteopast01 <matteopast@gmail.com>
2024-02-17 21:20:40 +0100

    addingo arduino code for imu reading

--- Arduino_src/sensors_and_imu_reading.ino
+++ Arduino_src/sensors_and_imu_reading.ino
@@ -1,59 +1,86 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_9DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_9DOF                dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+/* Update this with the correct SLP for accurate altitude measurements */
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+/**************************************************************************/
+/*!
+    @brief  Initialises all the sensors used by this example
+*/
+/**************************************************************************/
+void initImuSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+/**************************************************************************/
+
+
+
 // Pin per i sensori ultrasonici
 #define echoPinRight 4  // filo verde
 #define trigPinRight 3  // filo lilla
 #define echoPinLeft 8   // filo verde
 #define trigPinLeft 7   // filo lilla
 
-// Per la bussola
-#include <QMC5883LCompass.h>
-QMC5883LCompass compass;
 
-void setup() {
+
+
+/**************************************************************************/
+void setup(void)
+{
   Serial.begin(9600);
 
-  // Inizializzazione dei pin per i sensori ultrasonici
   pinMode(echoPinRight, INPUT);
   pinMode(trigPinRight, OUTPUT);
   pinMode(echoPinLeft, INPUT);
   pinMode(trigPinLeft, OUTPUT);
 
-  // Per la bussola
-  compass.init();
 
-  compass.setCalibrationOffsets(-197.00, -1316.00, -1411.00);
-  compass.setCalibrationScales(1.64, 0.87, 0.81);
+  
+  /* Initialise the sensors */
+  initImuSensors();
+
+
 }
 
-// Funzione per il sensore ultrasonico
-long sonarsensor(int triggerPin, int echoPin) {
-  long distance = 0;
-  digitalWrite(triggerPin, LOW);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  long times = pulseIn(echoPin, HIGH);
-  if (times < 38000) {
-    distance = 0.034 * times / 2;
-    return distance;
-  }
-  return distance;
-}
 
-void loop() {
-  // Legge la bussola
-  compass.read();
-  float degree = compass.getAzimuth();
-  // trasforma i gradi in formato 0°-360°
-  if (degree < 0) {
-    degree = degree + 360;
-    }
 
-  // Lettura e stampa dei valori dei sensori ultrasonici
+/**************************************************************************/
+/*!
+    @brief  Constantly check the roll/pitch/heading/altitude/temperature
+*/
+/**************************************************************************/
+void loop(void)
+{
+  
   Serial.print("#");
   Serial.print(sonarsensor(trigPinRight, echoPinRight));
   Serial.print("#");
   Serial.print(sonarsensor(trigPinLeft, echoPinLeft));
   // Stampa i gradi
   Serial.print("#");
-  Serial.println(degree);
+  Serial.println(imu_orientation_reading());
+
+  
 }
